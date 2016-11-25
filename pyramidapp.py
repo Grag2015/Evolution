6461a6949f91ca17bc7a17b62fbb8b82c45857b6
gmiholap <miholapgv@gmail.com>
2016-11-25 12:09:42 +0300

    изменил урл -  http://192.168.1.3:8888/section/

--- pyramidapp.py
+++ pyramidapp.py
@@ -19,7 +19,7 @@ def hello_world(request):
 
 config = Configurator()
 # The router instance creates a request object by mapping the request URL to URL patterns provided by the route mapper
-config.add_route('hello', '/') # maps a route to url и т.о. разные урлы могут обрабатываться разными функциями
+config.add_route('hello', '/section/') # maps a route to url и т.о. разные урлы могут обрабатываться разными функциями
 config.add_view(hello_world, route_name='hello') #The add_view method of the Configurator object maps a request object to a callable response object, using the hello_world method
 app = config.make_wsgi_app()
 
